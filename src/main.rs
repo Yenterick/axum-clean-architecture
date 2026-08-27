a7ed3bb3fc562a43a30bcae44830abcb43a9b586
Yenterick <yenterick@gmail.com>
2026-08-27 18:24:29 -0500

    feat: configuration and first endpoint implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Json, Router, response::IntoResponse, routing::get};
+use serde_json::{Value, json};
+
+#[tokio::main]
+async fn main() {
+    let app: Router = Router::new().route("/health", get(hello_world));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Server started successfully at 0.0.0.0:3000!");
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_world() -> impl IntoResponse {
+    let json_response: Value = json!({
+        "success": true,
+        "message": "Task manager is up and running!"
+    });
+    Json(json_response)
 }
